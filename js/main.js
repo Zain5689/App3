913d6a156537f4fe38f5f6a497be37d22254e653
Soha Ahmed <94359655+soha100@users.noreply.github.com>
2024-03-06 12:31:31 +0200

    App4

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ btn.onclick = function () {
     btn.innerHTML = "Show";
   }
 };
+
+// App4
+let img = document.getElementById("image");
+let images = [
+  "Image/bac.jpg",
+  "Image/wallpaperflare.com_wallpaper (3).jpg",
+  "Image/wallpaperflare.com_wallpaper.jpg",
+];
+let i = 0;
+function slideshow() {
+  img.setAttribute("src", images[i]);
+  if (i == images.length - 1) {
+    i = 0;
+  } else {
+    i++;
+  }
+  setTimeout(function () {
+    slideshow();
+  }, 2000);
+}
+slideshow();
